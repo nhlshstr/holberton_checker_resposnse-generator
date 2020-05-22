8fb82fe6c1e523dd647f01b05bdcae9643634039
christinepang1 <christinezpang@gmail.com>
2020-05-22 20:34:21 +0000

    Phrases seperated by levels

---
+++ phrases.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const level1 = [
+  'Segmentation FAAAAULT!!!',
+  'Big OOOF',
+  '...README...?',
+  'Checker didn’t choose you'
+];
+
+const level2 = [
+  'big NOPE!',
+  'Maybe you need some coffee.',
+  'Ugh, relying on me to debug your code...',
+  'Just because you run the checker again on the same code, doesn’t mean it’ll magically tell you something different...',
+  'What’s happening?',
+  'Checker had a bad dream',
+  'The checker is not broken',
+  '...typo?'
+];
+
+const level3 = [
+  'oof',
+  'Big Brain, Keep Going!',
+  'You got this!',
+  'Almost there!!',
+  'Ugh, relying on me to debug your code...',
+  'The checker is not broken',
+  '...typo?'
+];
+
+const level4 = [
+  'LET’S GOOOOOO',
+  'That was so fantastic, I can’t even',
+  'The Force is Strong in this one...',
+  'You === SmartyPants',
+  'That was beautiful',
+  'Oprah would approve.',
+  'Google would be jealous, nice job.',
+  'You 1... Errors 0!'
+];
